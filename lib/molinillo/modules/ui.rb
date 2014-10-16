1cf5519ac4be178dacf1f054d3d3c3c3f11d612b
Samuel E. Giddins <segiddins@segiddins.me>
2014-10-15 17:09:05 -0700

    [Documentation] Document the modules

--- lib/molinillo/modules/ui.rb
+++ lib/molinillo/modules/ui.rb
@@ -1,7 +1,16 @@
 module Molinillo
+  # Conveys information about the resolution process to a user.
   module UI
+    # Called roughly every second, this method should convey progress to the
+    # user.
+    #
+    # @return [void]
     def indicate_progress; end
 
+    # Conveys debug information to the user.
+    #
+    # @param [Integer] depth the current depth of the resolution process.
+    # @return [void]
     def debug(depth = 0)
       if ENV['CP_RESOLVER']
         debug_info = yield
