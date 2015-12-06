7c0a2cb07dc89232687d51e653fc8ed8bd6e64a7
Samuel Giddins <segiddins@segiddins.me>
2015-12-06 14:53:24 -0600

    Switch from bacon to rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -27,8 +27,5 @@ ROOT = Pathname.new(File.expand_path('../../', __FILE__))
 $LOAD_PATH.unshift((ROOT + 'lib').to_s)
 $LOAD_PATH.unshift((ROOT + 'spec').to_s)
 
-require 'bacon'
-require 'mocha-on-bacon'
-require 'pretty_bacon'
 require 'version_kit'
 require 'molinillo'
--- spec/state_spec.rb
+++ spec/state_spec.rb
@@ -4,7 +4,7 @@ module Molinillo
   describe ResolutionState do
     describe DependencyState do
       before do
-        @state = DependencyState.new(
+        @state = described_class.new(
           'name',
           %w(requirement1 requirement2 requirement3),
           DependencyGraph.new,
@@ -18,15 +18,11 @@ module Molinillo
       it 'pops a possibility state' do
         possibility_state = @state.pop_possibility_state
         %w(name requirements activated requirement conflicts).each do |attr|
-          possibility_state.send(attr).
-            should.equal @state.send(attr)
+          expect(possibility_state.send(attr)).to eq(@state.send(attr))
         end
-        possibility_state.is_a?(PossibilityState).
-          should.be.true?
-        possibility_state.depth.
-          should.equal @state.depth + 1
-        possibility_state.possibilities.
-          should.equal %w(possibility)
+        expect(possibility_state).to be_a(PossibilityState)
+        expect(possibility_state.depth).to eq(@state.depth + 1)
+        expect(possibility_state.possibilities).to eq(%w(possibility))
       end
     end
   end
