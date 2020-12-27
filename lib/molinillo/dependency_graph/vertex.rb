b517ecb66dc35bec17d1e84daf652067d8c392a1
Lukas Oberhuber <lukas.oberhuber@simplybusiness.co.uk>
2020-12-27 15:11:18 +0000

    Fix `vertex.inspect`
    This fails in bundler when doing `dependency_graph.inspect` as ruby and rubygems dependencies are
    named "Ruby\0" and "RubyGems\0" which
    causes a `ArgumentError` with message `string contains null byte`

--- lib/molinillo/dependency_graph/vertex.rb
+++ lib/molinillo/dependency_graph/vertex.rb
@@ -98,7 +98,7 @@ module Molinillo
 
       # @return [String] a string suitable for debugging
       def inspect
-        "#{self.class}:#{name}(#{payload.inspect})"
+        "#{self.class}:#{name}(#{payload})"
       end
 
       # @return [Boolean] whether the two vertices are equal, determined
